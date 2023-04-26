9d8b715974f721adc3420ba05e8b2357a91ef325
Nimo <limiu82214@gmail.com>
2023-04-26 23:30:41 +0800

    make interface for dataRetriever

--- internal/transformer/dataretriever/dataretriever.go
+++ internal/transformer/dataretriever/dataretriever.go
@@ -6,13 +6,16 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type IDataRetriever interface {
+	FetchMetadata() map[string]string
+}
+
 type dataRetriever struct {
 	url   string
 	bsoup *goquery.Document
 }
 
-// TODO: should use interface to make it testable and someday we can change to use other library
-func NewDataRetriever(videoUrl string) *dataRetriever {
+func NewDataRetriever(videoUrl string) IDataRetriever {
 	videoBsoup, err := goquery.NewDocument(videoUrl)
 	if err != nil {
 		log.Fatal(err)
