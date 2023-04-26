bfd90cfde2f34158d7045b6ca69d6c9d058663ba
Nimo <limiu82214@gmail.com>
2023-04-26 23:07:31 +0800

    ensure every dataRetriver should be create by New

--- internal/dataretriever/dataretriver.go
+++ internal/dataretriever/dataretriver.go
@@ -6,23 +6,23 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-type TubeVideoMetaDataRetriever struct {
+type tubeVideoMetaDataRetriever struct {
 	videoUrl   string
 	videoBsoup *goquery.Document
 }
 
-func NewVideoMetaDataRetriever(videoUrl string) *TubeVideoMetaDataRetriever {
+func NewVideoMetaDataRetriever(videoUrl string) *tubeVideoMetaDataRetriever {
 	videoBsoup, err := goquery.NewDocument(videoUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return &TubeVideoMetaDataRetriever{
+	return &tubeVideoMetaDataRetriever{
 		videoUrl:   videoUrl,
 		videoBsoup: videoBsoup,
 	}
 }
 
-func (t *TubeVideoMetaDataRetriever) MetaContentTags() *goquery.Selection {
+func (t *tubeVideoMetaDataRetriever) MetaContentTags() *goquery.Selection {
 	return t.videoBsoup.Find("meta")
 }
